82c1c6e7a7c46dfcee49ccb941824374e78569da
Daniel Macauyag <dmacauyag92@gmail.com>
2020-12-12 14:51:48 -0800

    starting to build out the site skeleton

---
+++ src/components/Header/index.js
@@ -0,0 +1,17 @@
+import * as React from "react"
+
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <h1>Daniel Macauyag</h1>
+        <p>I am a Full Stack Web Developer based in Los Angeles, CA</p>
+        <button>
+          <a>Learn More</a>
+        </button>
+      </div>
+    </header>
+  )
+}
+
+export default Header
